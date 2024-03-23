d7912c6710e9e197108071a044c7acde4260d101
Kinesis <lordkinesis@naver.com>
2024-03-24 00:44:06 +0900

    임시 세이브 Baram-Seminar-DL-Day1.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711122972',resources: [
+    cacheName: 'chirpy-1711208620',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
