fcd37f039e0baacbe911932d8d2828707ec54d2f
Kinesis <lordkinesis@naver.com>
2024-03-24 13:45:34 +0900

    Updating Baram-Seminar-DL-Day1.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711208620',resources: [
+    cacheName: 'chirpy-1711255245',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
