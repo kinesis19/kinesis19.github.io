706ef51c5b9b0324cbd4aacf18a39b134f245675
Kinesis <lordkinesis@naver.com>
2024-08-09 18:04:20 +0900

    :memo: Update: 2024-08-04-ATmega128_Concept_Summery.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723188516',resources: [
+    cacheName: 'chirpy-1723194199',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
