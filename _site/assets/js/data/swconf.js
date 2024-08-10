38674dde4f2cb3753c8d576b5ef474ad3a8d6847
Kinesis <lordkinesis@naver.com>
2024-08-10 16:57:11 +0900

    :memo: Delete: Image

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723276545',resources: [
+    cacheName: 'chirpy-1723276611',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
