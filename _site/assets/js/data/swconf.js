233fea6978e643d6c8b2b06f7e16c33834a4bdb5
Kinesis <lordkinesis@naver.com>
2024-09-02 18:57:34 +0900

    :wrench: Update: profile.png
    profile 이미지를 변경함.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725270305',resources: [
+    cacheName: 'chirpy-1725270909',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
