972b16d00b75bf59dc8a4c5a72b611dda423cc05
Kinesis <lordkinesis@naver.com>
2024-10-04 05:57:37 +0900

    :memo: Docs: Update 2024-10-03-Ubuntu_Install_with_usb.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1727974862',resources: [
+    cacheName: 'chirpy-1727988925',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
